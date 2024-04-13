f77f6ba1ad1a58882239d6b7557358607498830c
keerthanab23 <166646527+keerthanab23@users.noreply.github.com>
2024-04-12 19:25:18 -0500

    git commit -m "added vol_of_sphere.py"

---
+++ vol_sphere.py
@@ -0,0 +1,14 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:38) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+... def calculate_volume_of_sphere(radius):
+... 	pi = 3.14
+... 	volume = (4 / 3) * pi * radius * radius * radius
+... 	return volume
+... 
+... radius1 = 30
+... volume1 = calculate_volume_of_sphere(radius1)
+... print(f"The volume of a sphere with radius {radius1} is: {volume1}")
+... 
+... radius2 = 40
+... volume2 = calculate_volume_of_sphere(radius2)
